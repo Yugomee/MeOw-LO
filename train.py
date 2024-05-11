c01dc5eda1c44cd5752933e58465c1feae677b52
Yugomee <jeehyunee625@gmail.com>
2024-05-11 22:58:04 +0900

    Modified: Complete Trainloop

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import torch
 import torchvision
 import argparse
+import time
+from tqdm import tqdm
+from datetime import datetime
 from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
 from dataload import CatDataset
@@ -28,10 +31,10 @@ def main(args) :
     #TrainLoop
     model.train()
 
-    print("Train begins...")
-    for epoch in range(args.epochs):
-        epoch_loss = 0
-        for images in dataloader:
+    print(f"Train begins at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}...")
+    for epoch in tqdm(range(args.epochs), desc="Epoch Progress"):
+        total_loss = 0
+        for images in tqdm(dataloader, total=len(dataloader), desc=f"Epoch {epoch+1}/{args.epochs} ", leave=False):
             optimizer.zero_grad()
 
             # Each have same num of augmentations
@@ -39,31 +42,33 @@ def main(args) :
             # as list (batch_size, channel, img_size, img_size)
             anchors, positives, negatives = images
 
-            print("Check image size")
-            print(f'Anchor : {anchors[0].shape}, Positive : {positives[0].shape}')
+            # Apply model to each set of anchors, positives, and negatives for each cat
+            anchor_features = torch.cat([model(anchor.unsqueeze(0).to(device)) for a in anchors for anchor in a])
+            positive_features = torch.cat([model(positive.unsqueeze(0).to(device)) for p in positives for positive in p])
+            negative_features = torch.cat([model(negative.unsqueeze(0).to(device)) for n in negatives for negative in n])
 
-            # Move each tensor in the list to the device
-            anchors = [anchor.to(device) for anchor in anchors]
-            positives = [positive.to(device) for positive in positives]
-            negatives = [negative.to(device) for negative in negatives]
-
-            # Compute features for each set of images
-            anchor_features = torch.stack([model(anchor) for anchor in anchors])
-            positive_features = torch.stack([model(positive) for positive in positives])
-            negative_features = torch.stack([model(negative) for negative in negatives])
 
             # Compute the triplet loss
             loss = triplet_loss(anchor_features, positive_features, negative_features)
             loss.backward()
             optimizer.step()
-        print(f"Epoch {epoch+1}/{args.epochs} completed.")
+            total_loss += loss.item()
+
+            #print(f"Loss: {loss.item():.4f}, Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
+        print(f"Epoch {epoch+1}/{args.epochs} completed at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.")
+        avg_loss = total_loss / len(dataloader)
+        print(f'Epoch {epoch + 1} / {args.epochs} completed, Average Loss : {avg_loss:.4f}')
+
+    torch.save(model, f'{args.output_dir}/output_model.pt')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training a model on cat identicifation")
-    parser.add_argument('--directory', type=str, default='/data/etc/molo/CAT_00', help='Directory containing the dataset')
+    parser.add_argument('--directory', type=str, default='/data/etc/molo/CAT_train', help='Directory containing the dataset')
     parser.add_argument('--batch_size', type=int, default=4, help='Batch size for training')
     parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train')
     parser.add_argument('--debug', type= bool, default=False )
+    parser.add_argument('--output_dir', type=str)
 
 
     args = parser.parse_args()
