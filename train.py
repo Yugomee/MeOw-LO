e8c4adf667d64622db3230d5d9c7d205acc15fd4
Yugomee <jeehyunee625@gmail.com>
2024-05-26 14:49:49 +0900

    Modified : change avg validation and add normalization

--- train.py
+++ train.py
@@ -49,28 +49,34 @@ def extract_and_average_features(augmentations, model, device):
 
 def validate(model, dataloader, device):
     model.eval()  # Set model to evaluation mode
-    similarities = []
+    positive_similarities = []
+    negative_similarities = []
+
     with torch.no_grad():  # No gradients needed
-        for batch in dataloader:
+        for batch in tqdm(dataloader, desc = 'Validation process'):
             anchors, positives, negatives = batch
 
             # Extract features without gradient calculation
             anchor_features = extract_and_average_features(anchors, model, device)
             positive_features = extract_and_average_features(positives, model, device)
             negative_features = extract_and_average_features(negatives, model, device)
-
+            n_anchor_features = torch.nn.functional.normalize(anchor_features, p=2, dim=1)
+            n_positive_features = torch.nn.functional.normalize(positive_features, p=2, dim=1)
+            n_negative_features = torch.nn.functional.normalize(negative_features, p=2, dim=1)
             # Compute cosine similarity
-            pos_similarity = cosine_similarity(anchor_features, positive_features)
-            neg_similarity = cosine_similarity(anchor_features, negative_features)
+            pos_similarity = cosine_similarity(n_anchor_features, n_positive_features)
+            neg_similarity = cosine_similarity(n_anchor_features, n_negative_features)
 
-            # Append to list and later calculate average
-            similarities.append((pos_similarity.mean().item(), neg_similarity.mean().item()))
+             # Collect data for analysis
+            positive_similarities.extend(pos_similarity.tolist())
+            negative_similarities.extend(neg_similarity.tolist())
 
-    # Calculate average similarity scores across all batches
-    pos_sim_avg = sum(pos[0] for pos in similarities) / len(similarities)
-    neg_sim_avg = sum(neg[1] for neg in similarities) / len(similarities)
+    # Compute averages of collected data
+    pos_sim_avg = sum(positive_similarities) / len(positive_similarities) if positive_similarities else 0
+    neg_sim_avg = sum(negative_similarities) / len(negative_similarities) if negative_similarities else 0
 
     return pos_sim_avg, neg_sim_avg
+
 
 class Encoder(nn.Module):
     def __init__(self, pretrained=True, model_name="google/vit-base-patch16-224-in21k", embed_dim=128):
@@ -92,7 +98,7 @@ def save_model(model, path):
     torch.save(model.state_dict(), path)
 
 def main(args):
-    wandb.init(project="molo", entity="pai_jeehyun", config=args)
+    #wandb.init(project="molo", entity="pai_jeehyun", config=args)
     print("Registering Data...")
     dataset = CatDataset(directory=args.directory)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
@@ -103,8 +109,8 @@ def main(args):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = Encoder(pretrained=True, embed_dim=128).to(device)
-    triplet_loss = TripletMarginLoss(margin=1.0, p=2)
-    optimizer = AdamW(model.parameters(), lr=args.lr)
+    triplet_loss = TripletMarginLoss(margin=3.0, p=2)
+    optimizer = Adam(model.parameters(), lr=args.lr)
 
     model.train()
     print("Train begins...")
@@ -113,12 +119,18 @@ def main(args):
         for batch in tqdm(dataloader, desc="Training Batch", leave=False):
             optimizer.zero_grad()
             anchors, positives, negatives = batch
-
+            #print(f"Feature extracting starts at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
             anchor_features = extract_and_average_features(anchors, model, device)
             positive_features = extract_and_average_features(positives, model, device)
             negative_features = extract_and_average_features(negatives, model, device)
+            n_anchor_features = torch.nn.functional.normalize(anchor_features, p=2, dim=1)
+            n_positive_features = torch.nn.functional.normalize(positive_features, p=2, dim=1)
+            n_negative_features = torch.nn.functional.normalize(negative_features, p=2, dim=1)
+            #print(f"Feature extracting ends..Loss calculates starts at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+            loss = triplet_loss(n_anchor_features, n_positive_features, n_negative_features)
 
-            loss = triplet_loss(anchor_features, positive_features, negative_features)
+            #print(f"Loss calculates ends at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
             loss.backward()
             optimizer.step()
 
@@ -130,17 +142,17 @@ def main(args):
         avg_loss = total_loss / len(dataloader)
         print(f'Epoch {epoch + 1} / {args.epochs} completed, Average Loss : {avg_loss:.4f}')
 
-        wandb.log({"average_loss": avg_loss, "epoch": epoch+1})
+        #wandb.log({"average_loss": avg_loss, "epoch": epoch+1})
 
         pos_sim, neg_sim = validate(model, val_dataloader, device)
         print(f"Average Positive Similarity: {pos_sim:.4f}, Average Negative Similarity: {neg_sim:.4f}")
-        wandb.log({"positive similarity in validation": pos_sim, "epoch": epoch +1})
-        wandb.log({"nositive similarity in validation": neg_sim, "epoch": epoch +1})
+        #wandb.log({"positive similarity in validation": pos_sim, "epoch": epoch +1})
+        #wandb.log({"nositive similarity in validation": neg_sim, "epoch": epoch +1})
 
     torch.save(model, f'{args.output_dir}/output_model_{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.pt')
 
     print("Model saved successfully.")
-    wandb.finish()
+    #wandb.finish()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training a model on cat identification")
@@ -148,7 +160,7 @@ if __name__ == "__main__":
     parser.add_argument('--test_directory', type=str, default='/data/etc/molo/CAT_val', help = 'Directory for validation set')
     parser.add_argument('--batch_size', type=int, default=16, help='Batch size for training')
     parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train')
-    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')  # Adjusted type to float for learning rate
-    parser.add_argument('--output_dir', type=str)
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')  # Adjusted type to float for learning rate
+    parser.add_argument('--output_dir', type=str, default = '/data/etc/molo')
     args = parser.parse_args()
     main(args)
